245c1d36f3b5d886e7aa371f433729c77ca0cbef
Peshkov Dmitry <54577521+Dmitiyy@users.noreply.github.com>
2021-09-22 08:11:32 +0300

    Deleting config

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    mongodbConnection: 'mongodb+srv://fortulearnUser:TdBb8wHDt2wZOMpk@fortulearncluster.ogmmm.mongodb.net/fortuLearn?retryWrites=true&w=majority',
-    JWT_SECRET: '019201920192019209120do9102do12910920192019201920192019209'
-  }
-}
